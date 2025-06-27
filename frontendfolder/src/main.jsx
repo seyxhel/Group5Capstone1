ab7fd2369795f1a6bc11bd39a7bfa9941aafddbf
seyxhel <sealtieljosephbpelagio@iskolarngbayan.pup.edu.ph>
2025-06-27 12:00:40 +0800

    feat: Add console log for API URL in main entry point

--- frontendfolder/src/main.jsx
+++ frontendfolder/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
+console.log("API_URL:", import.meta.env.VITE_REACT_APP_API_URL);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <App />
