ed6657a5de5ed0388facbd81545d14ed37bcc8e9
seyxhel <sealtieljosephbpelagio@iskolarngbayan.pup.edu.ph>
2025-07-22 00:38:43 +0800

    fix: Add comment to clarify EMAIL_HOST_PASSWORD retrieval in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -157,7 +157,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD") #new one
 
 AUTH_USER_MODEL = 'core.Employee'
 USERNAME_FIELD = 'email'
